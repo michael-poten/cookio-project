02553562a380a71245ea1f82539515ab3349fc81
Michael Poten <michael.poten@gmx.de>
2026-04-01 22:44:56 +0200

    deploy: update webapp build

---
+++ docs/_nuxt/BC6wGsX5.js
@@ -0,0 +1,1 @@
+import{R as o}from"./s3Pv5y-1.js";import{M as t,u as a,W as r,J as n,L as u,o as c,x as i,P as p}from"./BBARC25d.js";import"./BvqvpnvI.js";import"./CWvYDcnX.js";import"./BuxPhbNV.js";const M={__name:"index",setup(m){t(),a(),r();const e=n(),{shuffleMode:s}=u(e);return c(async()=>{s.value=!1}),(f,_)=>(i(),p(o))}};export{M as default};
--- docs/_nuxt/DUqKbTbF.js
+++
@@ -1,1 +0,0 @@
-import{R as o}from"./CQ3j6Xfq.js";import{M as t,u as a,W as r,J as n,L as u,o as c,x as i,P as p}from"./CMSdEcG2.js";import"./BwmNMmM7.js";import"./BFNb2vGt.js";import"./D7kROMNY.js";const M={__name:"index",setup(m){t(),a(),r();const e=n(),{shuffleMode:s}=u(e);return c(async()=>{s.value=!1}),(f,_)=>(i(),p(o))}};export{M as default};
