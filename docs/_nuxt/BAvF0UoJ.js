fd7cd279724ba24b1754d2d38a766f45b6831774
Michael Poten <michael.poten@gmx.de>
2026-04-02 07:05:33 +0200

    deploy: update webapp build

--- docs/_nuxt/BAvF0UoJ.js
+++
@@ -1,1 +0,0 @@
-import{R as t}from"./s3Pv5y-1.js";import{M as o,I as r,W as a,J as n,L as u,o as c,x as i,P as p}from"./BBARC25d.js";import"./BvqvpnvI.js";import"./CWvYDcnX.js";import"./BuxPhbNV.js";const M={__name:"index",setup(m){o(),r(),a();const e=n(),{shuffleMode:s}=u(e);return c(async()=>{s.value=!0}),(_,f)=>(i(),p(t))}};export{M as default};
--- docs/_nuxt/CshB81fx.js
+++
@@ -1,1 +0,0 @@
-import{N as s,I as t,o as a,x as c,P as n}from"./BBARC25d.js";import{M as u}from"./BvqvpnvI.js";import"./CWvYDcnX.js";const l={__name:"index",setup(p){const e=s(),o=t(),r=e.query.id;return a(async()=>{r||o.push("/recipes")}),(i,_)=>(c(),n(u))}};export{l as default};
