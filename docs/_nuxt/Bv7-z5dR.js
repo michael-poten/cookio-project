ec7feaf4fe29e2c1d9a5373154bfc557e7a113f8
Michael Poten <michael.poten@gmx.de>
2026-04-04 15:31:18 +0200

    deploy: update webapp build

--- docs/_nuxt/Bv7-z5dR.js
+++
@@ -1,1 +0,0 @@
-import{R as o}from"./C7XFtzJy.js";import{M as t,u as a,W as r,J as n,L as u,o as c,x as i,P as p}from"./CBuLLFCv.js";import"./BL7ALIFt.js";import"./BwCYPPcD.js";import"./WvYQwvhx.js";const M={__name:"index",setup(m){t(),a(),r();const e=n(),{shuffleMode:s}=u(e);return c(async()=>{s.value=!1}),(f,_)=>(i(),p(o))}};export{M as default};
