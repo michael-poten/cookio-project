525897bde1bea3c49c392da03c648e7346858ac9
Michael Poten <michael.poten@gmx.de>
2026-04-07 16:27:02 +0200

    deploy: update webapp build

--- docs/_nuxt/ApooZBuF.js
+++
@@ -1,1 +0,0 @@
-import{a4 as t}from"./Ctj1l6NE.js";function f(r){return r?t(r).format("DD.MM."):"-"}function o(r){return r?t(r).format("DD.MM.YYYY"):"-"}export{o as a,f};
---
+++ docs/_nuxt/CjIL7lWr.js
@@ -0,0 +1,1 @@
+import{a4 as t}from"./DpEOW8Gk.js";function f(r){return r?t(r).format("DD.MM."):"-"}function o(r){return r?t(r).format("DD.MM.YYYY"):"-"}export{o as a,f};
