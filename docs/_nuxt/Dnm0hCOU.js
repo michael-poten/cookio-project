e8cb42d1dacf1328dbc6409e9b7b31b71bc74f10
Michael Poten <michael.poten@gmx.de>
2026-04-08 11:37:45 +0200

    deploy: update webapp build

---
+++ docs/_nuxt/Dnm0hCOU.js
@@ -0,0 +1,1 @@
+import{N as s,I as t,o as a,x as c,P as n}from"./FngRmThi.js";import{M as u}from"./BE_P2XY8.js";import"./CIEwsAR4.js";const l={__name:"index",setup(p){const e=s(),o=t(),r=e.query.id;return a(async()=>{r||o.push("/recipes")}),(i,_)=>(c(),n(u))}};export{l as default};
