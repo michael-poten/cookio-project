7b39f6228950b18d6eacbf556a0c1d7a05131704
Michael Poten <michael.poten@gmx.de>
2026-04-11 09:03:17 +0200

    deploy: update webapp build

---
+++ docs/_nuxt/Bl1ZfEWW.js
@@ -0,0 +1,1 @@
+import{x as e,y as t}from"./BnoFd6Fg.js";const _={__name:"image",setup(a){return(r,c)=>(e(),t("div"))}};export{_ as default};
---
+++ docs/_nuxt/BoSbcMVv.js
@@ -0,0 +1,1 @@
+import{a9 as t}from"./BnoFd6Fg.js";function f(r){return r?t(r).format("DD.MM."):"-"}function o(r){return r?t(r).format("DD.MM.YYYY"):"-"}export{o as a,f};
